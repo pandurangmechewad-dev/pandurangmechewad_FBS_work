736d7995ef3362506cecfe298c7911029c204c11
pandurangmechewad-dev <pandurangmechewad@gmail.com>
2026-02-17 15:24:52 +0530

    Delete Asignment 2 directory

--- Asignment 2/que2.py
+++
@@ -1,8 +0,0 @@
-#2. Convert temp from Celsius to Fahrenheit. (C/5 = (F-32)/9)
-
-
-celsius = float(input("Enter temperature in Celsius: "))
-
-fahrenheit = (9 * celsius) / 5 + 32
-
-print("Temperature in Fahrenheit is:", fahrenheit)
--- Asignment 2/que5.py
+++
@@ -1,8 +0,0 @@
-#5. WAP to calculate selling price of book based on cost price and discount.
-
-cp = float(input("Enter cost price: "))
-d = float(input("Enter discount (%): "))
-
-sp = cp - (cp * d / 100)
-
-print("Selling Price =", sp)
